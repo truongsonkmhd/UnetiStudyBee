753ce713fbabb697c5bda7fa1039ba2974e2c61e
truong son <truongsonkmhd@gmail.com>
2025-08-22 15:17:13 +0700

    update 22/8

--- src/main/java/com/truongsonkmhd/unetistudy/configuration/PreFilter.java
+++ src/main/java/com/truongsonkmhd/unetistudy/configuration/PreFilter.java
@@ -4,14 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Configuration
+@RequiredArgsConstructor
 @Slf4j(topic = "CUSTOMIZE-REQUEST-FILTER ")
+//@EnableGlobalAuthentication(prePostEnable = true)
 public class PreFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
--- src/main/java/com/truongsonkmhd/unetistudy/security/MyUserDetail.java
+++ src/main/java/com/truongsonkmhd/unetistudy/security/MyUserDetail.java
@@ -1,26 +1,22 @@
 package com.truongsonkmhd.unetistudy.security;
 
+import com.truongsonkmhd.unetistudy.model.Role;
 import com.truongsonkmhd.unetistudy.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
-public class MyUserDetail implements UserDetails {
-    private final User user;
-
-    public MyUserDetail(User user) {
-        this.user = user;
-    }
-
-    public User user() {
-        return user;
-    }
+public record MyUserDetail(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<Role> roleList = user.getRoles().stream().toList();
+
+        List<String> roleName = user.getRoles().stream().map(Role::getName).toList();
         return user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
                 .collect(Collectors.toList());
